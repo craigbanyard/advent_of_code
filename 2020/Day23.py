68dc3015d119875f7a10be0465c93e858f1db622
craigbanyard <76853689+craigbanyard@users.noreply.github.com>
2021-01-03 12:52:01 +0000

    Update Day23.py

--- 2020/Day23.py
+++ 2020/Day23.py
@@ -1,77 +1,66 @@
 from helper import aoc_timer
 
 
-def hash_cups(data, mod=9):
-    data = str(data)
-    mx, last = len(data), int(data[-1])
-    cups = {int(a): int(b) for a, b in zip(data, data[1:])}
-    while mod > mx:
-        cups[last] = mx + 1
-        last = (mx := mx + 1)
-    cups[last] = int(data[0])
+def get_cups(data, mod):
+    cups = [None] * ((mx := len(data)) + 1)
+    last = int(data[-1])
+    for idx, cup in enumerate(data, start=1):
+        cups[int(cup)] = int(data[idx % mx])
+    if mod > mx:
+        cups[last] = (mx := mx + 1)    
+        cups += list(range(mx + 1, mod + 1)) + [int(data[0])]
     return cups
 
 
-def mod_inc(n, i=1, m=9):
-    ans = (n + i) % m
-    if not ans:
-        ans = m
-    return ans
+def dec_mod(n, m):
+    return n - 1 or m
 
 
-def turn(cup, cups, mod=9):
-    pick = []
-    dest = mod_inc(cup, -1, mod)
-    for _ in range(3):
-        # cup = cups[cup]
-        pick.append(cup := cups[cup])
-        while dest in pick:
-            dest = mod_inc(dest, -1, mod)
-    return tuple(pick), cups[cup], dest, cups[dest]
+def turn(cup, cups, mod):
+    dest = dec_mod(cup, mod)
+    p1 = cups[cup]
+    p2 = cups[p1]
+    p3 = cups[p2]
+    while dest == p1 or dest == p2 or dest == p3:
+        dest = dec_mod(dest, mod)
+    return p1, p3, cups[p3], dest, cups[dest]
 
 
-def p1_ans(cups, cup=1, mod=9):
+def p1(cups, cup=1, mod=9):
     ans = ''
     for _ in range(mod - 1):
-        cup = cups[cup]
-        ans += str(cup)
-    return ans
-
-
-def p2_ans(cups, cup=1, n=2):
-    ans = 1
-    for _ in range(n):
-        ans *= (cup := cups[cup])
+        ans += str((cup := cups[cup]))
     return ans
 
 
 @aoc_timer
-def play(data, mod=9, moves=100, part1= True, output=False):
-    cups = hash_cups(data, mod)
-    cup = int(str(data)[0])
+def Day23(data, mod=9, moves=100, part1=True):
+    cups = get_cups(data, mod)
+    cup = int(data[0])
     for m in range(moves):
-        pick, ncup, a, b = turn(cup, cups, mod)
-        if output:
-            print(f"-- move {m+1} --\n({cup}), {cups}\npick up: {pick}\n" +
-                  f"next cup: ({ncup})\ndestination: {a}\n")
+        p, q, ncup, a, b = turn(cup, cups, mod)
         cups[cup] = ncup
-        cups[a] = pick[0]
-        cups[pick[-1]] = b
+        cups[a] = p
+        cups[q] = b
         cup = ncup
-    if output:
-        print(f"-- final --\ncups: ({cup}) {cups}")
     if part1:
-        return p1_ans(cups)
-    return p2_ans(cups)
+        return p1(cups)
+    return (a := cups[1]) * cups[a]
 
 
 # %% Output
 def main():
-    data = 589174263
+    data = '589174263'
     print("AoC 2020\nDay 23")
-    print("Part 1:", play(data))
-    print("Part 2:", play(data, 1000000, 10000000, False, False))
+    print("Part 1:", Day23(data))
+    print("Part 2:", Day23(data, 1000000, 10000000, False))
 
 
 if __name__ == '__main__':
     main()
+
+
+'''
+%timeit Day23(data)
+85.3 µs ± 78.2 ns per loop (mean ± std. dev. of 7 runs, 10000 loops each)
+'''
