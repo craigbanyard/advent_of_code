3eea0e350cdbb649cbe90782a603cac0013027f1
craigbanyard <76853689+craigbanyard@users.noreply.github.com>
2021-01-05 18:40:26 +0000

    Update Day02.py

--- 2020/Day02.py
+++ 2020/Day02.py
@@ -1,29 +1,15 @@
-from time import time
+from helper import aoc_timer
 import re
 from os import getcwd
 
 
-def re_split(delimiters, string):
-    regexPattern = '|'.join(map(re.escape, delimiters))
-    return re.split(regexPattern, string)
-
-
-def to_int(s):
-    try:
-        return int(s)
-    except ValueError:
-        return s
-
-
-def get_input(path):
-    delims = [': ', '-', ' ']
-    parse = lambda x: re_split(delims, x.strip())
-    return [list(map(to_int, parse(x))) for x in open(path).readlines()]
-
-
-def Day02(data):
+@aoc_timer
+def Day02(path):
     p1, p2 = 0, 0
-    for lo, hi, x, pwd in data:
+    for line in open(path).read().split('\n'):
+        x, pwd = line.split(': ')
+        lo, hi = map(int, re.findall('\d+', x))
+        x = x[-1]
         if lo <= pwd.count(x) <= hi:
             p1 += 1
         if (pwd[lo-1] == x) ^ (pwd[hi-1] == x):
@@ -34,15 +20,10 @@ def Day02(data):
 # %% Output
 def main():
     path = getcwd() + "\\Inputs\\Day02.txt"
-    print("AoC 2020\nDay 2\n-----")
-    t0 = time()
-    data = get_input(path)
-    print("Data:", time() - t0, "\n-----")
-    t0 = time()
-    p1, p2 = Day02(data)
+    print("AoC 2020\nDay 2")
+    p1, p2 = Day02(path)
     print("Part 1:", p1)
     print("Part 2:", p2)
-    print("Time:", time() - t0, "\n-----")
 
 
 if __name__ == '__main__':
@@ -50,9 +31,6 @@ if __name__ == '__main__':
 
 
 '''
-%timeit get_input(path)
-11 ms ± 243 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
-
-%timeit Day02(data)
-527 µs ± 4.18 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
+%timeit Day02(path)
+3.52 ms ± 3.91 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
 '''
