b2ea937167e4cc84e901bec64fd2be1d287fa22d
craigbanyard <76853689+craigbanyard@users.noreply.github.com>
2021-01-11 22:48:32 +0000

    Tidy and speed up Day 12
    Still using dictionary of N, E, S, W
    Should next try replacing everything with complex numbers (x -> real, y -> imaginary)

--- 2020/Day12.py
+++ 2020/Day12.py
@@ -13,12 +13,23 @@ def rotate(c, d, u):
     return clock[(clock.index(c) + turn[d]) % 4]
 
 
+def rotate_w(w, d, u):
+    # Reduce to (x, y) = (E, N)
+    w['N'] -= w['S']
+    w['E'] -= w['W']
+    w['S'] = w['W'] = 0
+    turn = {'L': -u//90, 'R': u//90}
+    for _ in range(turn[d] % 4):
+        w['E'], w['N'] = w['N'], -w['E']
+    return w
+
+
 @aoc_timer
 def Day12(data, part1=True):
 
     # Relevant for both parts:
     rots = {'L', 'R'}
-    dirs = {k: 0 for k in 'NSEW'}
+    dirs = {k: 0 for k in 'NESW'}
 
     if part1:
         curr = 'E'
@@ -31,13 +42,10 @@ def Day12(data, part1=True):
                 continue
             dirs[head] += unit
     else:
-        wayp = {'N': 1, 'S': 0, 'E': 10, 'W': 0}
+        wayp = {'N': 1, 'E': 10, 'S': 0, 'W': 0}
         for head, unit in data:
             if head in rots:
-                new = {k: 0 for k in 'NSEW'}
-                for h in wayp:
-                    new[rotate(h, head, unit)] = wayp[h]
-                wayp = new
+                wayp = rotate_w(wayp, head, unit)
                 continue
             if head == 'F':
                 for h in wayp:
@@ -71,5 +79,5 @@ if __name__ == '__main__':
 217 µs ± 675 ns per loop (mean ± std. dev. of 7 runs, 1000 loops each)
 
 %timeit Day12(data, False)
-864 µs ± 2.84 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
+567 µs ± 1.18 µs per loop (mean ± std. dev. of 7 runs, 1000 loops each)
 '''
