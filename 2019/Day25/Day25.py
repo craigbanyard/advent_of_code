4736ace3c154bf57b1e411cde11d0f04933dd18e
craigbanyard <76853689+craigbanyard@users.noreply.github.com>
2021-12-17 15:31:51 +0000

    Add Colours class to helper library

--- 2019/Day25/Day25.py
+++ 2019/Day25/Day25.py
@@ -1,49 +1,8 @@
-from helper import aoc_timer
+from helper import aoc_timer, Colours
 from intcode import IntcodeComputer
 from collections import deque
 import sys
 import re
-
-
-class Colours:
-    """ANSI code class for terminal highlighting."""
-
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    HIDE = '\033[8m'
-    STRIKE = '\033[9m'
-
-    class fg:
-        """Foreground colours."""
-
-        BLACK = '\033[30m'
-        RED = '\033[31m'
-        GREEN = '\033[32m'
-        ORANGE = '\033[33m'
-        BLUE = '\033[34m'
-        PURPLE = '\033[35m'
-        CYAN = '\033[36m'
-        LIGHTGREY = '\033[37m'
-        DARKGREY = '\033[90m'
-        LIGHTRED = '\033[91m'
-        LIGHTGREEN = '\033[92m'
-        YELLOW = '\033[93m'
-        LIGHTBLUE = '\033[94m'
-        PINK = '\033[95m'
-        LIGHTCYAN = '\033[96m'
-
-    class bg:
-        """Background colours."""
-
-        BLACK = '\033[40m'
-        RED = '\033[41m'
-        GREEN = '\033[42m'
-        ORANGE = '\033[43m'
-        BLUE = '\033[44m'
-        PURPLE = '\033[45m'
-        CYAN = '\033[46m'
-        LIGHTGREY = '\033[47m'
 
 
 class Day25:
--- Modules/helper/helper.py
+++ Modules/helper/helper.py
@@ -4,6 +4,47 @@ import numpy as np
 import math
 
 
+class Colours:
+    """ANSI code class for terminal highlighting."""
+
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    HIDE = '\033[8m'
+    STRIKE = '\033[9m'
+
+    class fg:
+        """Foreground colours."""
+
+        BLACK = '\033[30m'
+        RED = '\033[31m'
+        GREEN = '\033[32m'
+        ORANGE = '\033[33m'
+        BLUE = '\033[34m'
+        PURPLE = '\033[35m'
+        CYAN = '\033[36m'
+        LIGHTGREY = '\033[37m'
+        DARKGREY = '\033[90m'
+        LIGHTRED = '\033[91m'
+        LIGHTGREEN = '\033[92m'
+        YELLOW = '\033[93m'
+        LIGHTBLUE = '\033[94m'
+        PINK = '\033[95m'
+        LIGHTCYAN = '\033[96m'
+
+    class bg:
+        """Background colours."""
+
+        BLACK = '\033[40m'
+        RED = '\033[41m'
+        GREEN = '\033[42m'
+        ORANGE = '\033[43m'
+        BLUE = '\033[44m'
+        PURPLE = '\033[45m'
+        CYAN = '\033[46m'
+        LIGHTGREY = '\033[47m'
+
+
 def aoc_timer(func=None, *, repeat=1, metric=min, **margs):
     """
     Decorator that times the call of a function, func, and prints its execution time.
