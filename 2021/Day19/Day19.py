788a76b66de8430d74d3d3f5c19c0ecd58c31854
Craig Banyard <craig.banyard@rpc-tyche.com>
2021-12-21 23:50:06 +0000

    Day 19 Part 2

--- 2021/Day19/Day19.py
+++ 2021/Day19/Day19.py
@@ -1,5 +1,5 @@
 from helper import aoc_timer
-from collections import Counter, defaultdict, deque
+from collections import defaultdict, deque
 import itertools as it
 from matplotlib import pyplot as plt
 
@@ -55,12 +55,12 @@ def sub(a, b):
 
 def euc(a, b):
     '''Return the sqaure of the Euclidean distance between points a and b.'''
-    return sum((p - q) ** 2 for p, q in zip(a, b))
+    return sum(map(lambda d: pow(d, 2), sub(a, b)))
 
 
 def manhattan(a, b):
     '''Return the Manhattan distance between points a and b.'''
-    return sum(abs(p - q) for p, q in zip(a, b))
+    return sum(map(abs, sub(a, b)))
 
 
 def tri(coords):
@@ -69,7 +69,7 @@ def tri(coords):
 
 
 def draw_beacons(beacons):
-    '''Render 3D plot of beacons, colour coded by region.'''
+    '''Render 3D plot of beacons, colour-coded by region.'''
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
     colours = ['r', 'g', 'b', 'c', 'm', 'y', 'darkorange', 'lime', 'k']
@@ -100,26 +100,13 @@ def orientations(coords):
     ]
 
 
-def inverses(coords):
-    '''Return a generator of orientations of coords in 3D space.'''
-    x, y, z = coords
-    yield from [
-        (+x, +y, +z), (+x, -z, +y), (+x, -y, -z), (+x, +z, -y),
-        (+y, +x, -z), (+z, +x, +y), (-y, +x, +z), (-z, +x, -y),
-        (+y, +z, +x), (-z, +y, +x), (-y, -z, +x), (+z, -y, +x),
-        (-x, +y, -z), (-x, +z, +y), (-x, -y, +z), (-x, -z, -y),
-        (+y, -x, +z), (-z, -x, +y), (-y, -x, -z), (+z, -x, -y),
-        (+y, -z, -x), (+z, +y, -x), (-y, +z, -x), (-z, -y, -x)
-    ]
-
-
 def nth(iterable, n, default=None):
     '''Returns the nth item of the iterable or a default value.'''
     return next(it.islice(iterable, n, None), default)
 
 
-def nth_triangle(triangle, n, f=orientations):
-    return tuple([nth(f(p), n) for p in triangle])
+def nth_triangle(triangle, n):
+    return tuple([nth(orientations(p), n) for p in triangle])
 
 
 def translation(a, b):
@@ -142,11 +129,6 @@ def translation(a, b):
     return None, None
 
 
-def print_dict(dict: dict) -> None:
-    for k, v in dict.items():
-        print(f'{k}: {v}')
-
-
 @aoc_timer
 def Day19(data):
     '''
@@ -159,99 +141,67 @@ def Day19(data):
         triangles (12 beacons) are visible to both scanners.
     4.  A scanner can never detect another scanner's 'anchor' beacons.
     5.  We can determine the overlapping triangles via the SSS congruence
-        rule, which states that if all sides are equal, the triangles are
-        equal.
+        rule, which states that if two triangles have matching side
+        lengths, the triangles are equal (congruent).
     6.  For part 1, we count the unique triangles and add the anchors.
-    7.  I assume part 2 will require us to actually orient the map. Joy.
+    7.  Part 2 requires us to actually orient the map. Joy.
     '''
-    A = {0: (0, 0, 0)}      # Positions  s: (x, y, z)
-    M = defaultdict(set)    # Map        s: {s1, s2, ...}
-    S = defaultdict(set)    # Sensors    s: {t1, t1, ..., t12}
-    T = defaultdict(list)   # Triangles  t: [(s1, xyz1), (s2, xyz2), ...]
-    P = [{} for _ in data]  # Point map  [{t1: xyz1, t2: xyz2, ...}, ...]
+    A = {0: (0, 0, 0)}      # Sensor positions  s: (x, y, z)
+    M = defaultdict(set)    # Overlap map       s: {s1, s2, ...}
+    S = defaultdict(set)    # Sensor FoV        s: {t1, t1, ..., t12}
+    T = set()               # Unique triangles  {t1, t2, ...}
+    P = [{} for _ in data]  # Triangle coords   [{t1: xyz1, t2: xyz2, ...}, ...]
     p1 = 0
     for s, beacons in data.items():
         num_anchors = len(beacons) - 24
         anchors, triangles = beacons[:num_anchors], beacons[num_anchors:]
         for t in it.zip_longest(*[iter(triangles)] * 3):
             triangle = tri(t)
-            T[triangle].append((s, list(t)))
+            T.add(triangle)
             S[s].add(triangle)
             P[s][triangle] = t
         p1 += len(anchors)
     p1 += 3 * len(T)
 
-    # TODO: Part 2
-    # General idea is to start with scanner 0 and orient all others
-    # relative to this one. Do this by applying rotations to triangles
-    # until all 12 distances match for all coordinates - this is then
-    # the distance between the scanners.
-    # Continue via linked scanners, each time orienting relative to
-    # scanner 0.
-
     # Overlapping bounding cubes - four triangles shared between two sensors
-    SHARED = {}
+    B = defaultdict(set)
     for r, s in it.combinations(S.keys(), 2):
         if len((common_triangles := S[r] & S[s])) == 4:
             M[r].add(s)
             M[s].add(r)
-            SHARED[r] = common_triangles
-            SHARED[s] = common_triangles
-    # print_dict(M)
+            B[r] |= common_triangles
+            B[s] |= common_triangles
 
-    # For each overlapping pair of sensors, take one shared triangle and
-    # use this to orient the second sensor relative to the first
+    # For each overlapping pair of sensors, take one shared triangle
+    # and use this to orient the second sensor relative to the first.
+    # If this can't be done, move to the next shared triangle.
     Q = deque([(0, 0)])
     while Q:
         s1, k1 = Q.popleft()
-        print(f'{s1}: {k1}')
-        for triangle_id in SHARED[s1]:
-            if triangle_id not in P[s1]:
-                continue
-            t1 = P[s1][triangle_id]
+        for triangle_id in B[s1]:
+            t1 = nth_triangle(P[s1][triangle_id], k1)
             for s2 in M[s1]:
                 if s2 in A:
                     continue
                 if triangle_id not in P[s2]:
                     continue
-                # t2 = nth_triangle(X[s2][tid], k1, f=inverses)
                 t2 = P[s2][triangle_id]
                 dist, k2 = translation(t1, t2)
                 if dist is not None:
-                    A[s2] = add(A[s1], nth(inverses(dist), k1))
+                    A[s2] = add(A[s1], dist)
                     Q.append((s2, k2))
-    print_dict(A)
+    p2 = max(it.starmap(manhattan, it.combinations(A.values(), 2)))
 
-    print("old method below")
-    D = {}
-    for k, v in T.items():
-        # print(f'{k}: {v}')
-        if len(v) == 1:
-            continue
-        for (s1, t1), (s2, t2) in it.combinations(v, 2):
-            if s2 not in M[s1]:
-                continue
-            if (s1, s2) in D:
-                continue
-            if (t := translation(t1, t2)) is not None:
-                D[(s1, s2)] = t
-    print_dict(D)
-
-    return p1, None
+    return p1, p2
 
 
 # %% Output
 def main():
     print("AoC 2021\nDay 19")
     data = get_input('input.txt')
-    data = get_input('sample.txt')  # 79
-    # data = get_input('sample2.txt')  # 465
     p1, p2 = Day19(data)
     print("Part 1:", p1)
     print("Part 2:", p2)
-    # for _, beacons in data.items():
-    #     test_plot(beacons)
-    # draw_beacons(data[0])
 
 
 if __name__ == '__main__':
