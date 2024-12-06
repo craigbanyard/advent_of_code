41f79166e08f85a3cc490f0d57866f17e5e7c322
Craig Banyard <craig.banyard@rpc-tyche.com>
2024-12-06 22:19:51 +0000

    Optimise by truncating repeated paths

--- 2024/Day06/Day06.py
+++ 2024/Day06/Day06.py
@@ -5,7 +5,8 @@ from helper import aoc_timer
 class Map:
     def __init__(self, path: str) -> None:
         self.path = path
-        self.visited = set()
+        self.visited = {}
+        self.p2 = 0
         self.get_input()
 
     @aoc_timer
@@ -16,40 +17,47 @@ class Map:
             "v": 1j,
             "<": -1+0j,
         }
-        self.W = set()
+        self.obs = set()
         with open(self.path) as f:
             for r, line in enumerate(f.read().splitlines()):
                 for c, ch in enumerate(line):
                     if ch == "#":
-                        self.W.add(complex(c, r))
-                    elif (s := D.get(ch)):
-                        self.d, self.pos = s, complex(c, r)
+                        self.obs.add(complex(c, r))
+                    elif d := D.get(ch):
+                        self.d, self.pos = d, complex(c, r)
             self.R, self.C = r + 1, len(line)
 
-    def patrol(self, walls: set[complex] | None = None, log: bool = True) -> bool:
-        walls = walls or self.W.copy()
-        hits = set()
-        while 0 <= self.pos.imag < self.R and 0 <= self.pos.real < self.C:
-            if log:
-                self.visited.add(self.pos)
-            if (pos := self.pos + self.d) in walls:
-                if (self.d, pos) in hits:
-                    return True
-                hits.add((self.d, pos))
-                self.d *= 1j
+    def patrol(
+        self,
+        d: complex | None = None,
+        pos: complex | None = None,
+        obs: set[complex] | None = None,
+        log: bool = True,
+    ) -> None:
+        d = d or self.d
+        pos = pos or self.pos
+        obs = obs or self.obs
+        turns = set()
+        while 0 <= pos.imag < self.R and 0 <= pos.real < self.C:
+            if log and pos not in self.visited:
+                self.visited[pos] = (d, pos - d)
+            if (new_pos := pos + d) in obs:
+                if (d, new_pos) in turns:
+                    self.p2 += 1
+                    return
+                turns.add((d, new_pos))
+                d *= 1j
                 continue
-            self.pos = pos
-        return False
+            pos = new_pos
 
     @aoc_timer
     def solve(self) -> tuple[int, int]:
-        d, pos = self.d, self.pos
         self.patrol()
-        p2 = 0
-        for p in self.visited:
-            self.d, self.pos = d, pos
-            p2 += self.patrol(self.W | {p}, log=False)
-        return len(self.visited), p2
+        for new_obs, (d, pos) in self.visited.items():
+            if new_obs == self.pos:
+                continue
+            self.patrol(d, pos, self.obs | {new_obs}, log=False)
+        return len(self.visited), self.p2
 
 
 def main() -> None:
