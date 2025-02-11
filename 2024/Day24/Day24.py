48f2cdbde418d40ed6a28b7aa12f2646ea6fe893
Craig Banyard <craig.banyard@rpc-tyche.com>
2025-02-11 23:43:42 +0000

    Generalised 24.2 solution

--- 2024/Day24/Day24.py
+++ 2024/Day24/Day24.py
@@ -1,42 +1,62 @@
 # %% Day 24
 from helper import aoc_timer
 from collections import deque
-import itertools as it
+from dataclasses import dataclass
 from operator import and_, or_, xor
-from typing import Callable
+from typing import Iterator
 
-type Wires = dict[str, int]
-type Gates = dict[str, tuple[Callable, str, str]]
+
+@dataclass
+class Gate:
+    f_: str
+    a: str
+    b: str
+
+    def __post_init__(self) -> None:
+        self.f = {"AND": and_, "OR": or_, "XOR": xor}[self.f_]
+        self.a, self.b = sorted([self.a, self.b])
+
+    def __repr__(self) -> str:
+        return f"{self.a} {self.f_} {self.b}"
+
+    def __hash__(self) -> int:
+        return hash((self.f_, self.a, self.b))
+
+    def __iter__(self) -> Iterator:
+        yield from (self.f_, self.a, self.b)
 
 
 class System:
     def __init__(self, *, path: str = "", x: int = 0, y: int = 0) -> None:
-        self.path = path
-        if self.path:
+        if path:
+            self.path = path
             self.get_input()
             self.read_bits()
-            return
-        self.x = x
-        self.y = y
-        self.z = x + y
-        self.bits = max(map(int.bit_length, (x, y)))
-        self.set_wires()
-        self.set_gates()
+        else:
+            self.x = x
+            self.y = y
+            self.z = x + y
+            self.bits = max(map(int.bit_length, (x, y)))
+            self.set_wires()
+            self.set_gates()
+        self.gates_ = self.invert_gates()
+        self.errors = set()
 
     def __repr__(self) -> str:
         return f"x={self.x}, y={self.y}\n" + "\n".join(
-            f"{a} {f.__name__.upper().removesuffix("_")} {b} -> {c}"
-            for c, (f, a, b) in self.gates.items()
+            f"{v} -> {k}" for k, v in self.gates.items()
         )
 
     @staticmethod
-    def parse_wires(w: str) -> Wires:
+    def parse_wires(w: str) -> dict[str, int]:
         return {k: int(v) for k, v in [line.split(": ") for line in w.splitlines()]}
 
     @staticmethod
-    def parse_gates(g: str) -> Gates:
-        F = {"AND": and_, "OR": or_, "XOR": xor}
-        return {c: (F[f], a, b) for a, f, b, _, c in [*map(str.split, g.splitlines())]}
+    def parse_gates(g: str) -> dict[str, Gate]:
+        return {c: Gate(f, a, b) for a, f, b, _, c in [*map(str.split, g.splitlines())]}
+
+    def invert_gates(self) -> dict[Gate, str]:
+        return {v: k for k, v in self.gates.items()}
 
     @aoc_timer
     def get_input(self) -> None:
@@ -65,60 +85,100 @@ class System:
             }
 
     def set_gates(self) -> None:
-        self.gates = {"z00": (xor, "x00", "y00"), "c01": (and_, "x00", "y00")}
+        self.gates = {
+            "z00": Gate("XOR", "x00", "y00"),
+            "c01": Gate("AND", "x00", "y00"),
+        }
         if self.bits < 2:
-            self.gates["z01"] = (and_, "c01", "c01")
+            self.gates["z01"] = Gate("OR", "c01", "c01")
             return
         for idx in range(1, self.bits):
-            m = f"{idx:02d}"
-            n = f"{(idx + 1):02d}"
-            self.gates[f"m{m}"] = (xor, f"x{m}", f"y{m}")
-            self.gates[f"a{n}"] = (and_, f"x{m}", f"y{m}")
-            self.gates[f"z{m}"] = (xor, f"m{m}", f"c{m}")
-            self.gates[f"b{n}"] = (and_, f"m{m}", f"c{m}")
-            self.gates[f"c{n}"] = (or_, f"a{n}", f"b{n}")
-        self.gates[f"z{n}"] = (or_, f"a{n}", f"b{n}")
-        self.gates.pop(f"c{n}", None)
+            X, Y, Z, M, C = [f"{r}{idx:02d}" for r in "xyzmc"]
+            A, B, C_, Z_ = [f"{r}{(idx + 1):02d}" for r in "abcz"]
+            self.gates[M] = Gate("XOR", X, Y)
+            self.gates[A] = Gate("AND", X, Y)
+            self.gates[Z] = Gate("XOR", M, C)
+            self.gates[B] = Gate("AND", M, C)
+            self.gates[C_] = Gate("OR", A, B)
+        self.gates[Z_] = Gate("OR", A, B)
+        self.gates.pop(C_, None)
 
     @aoc_timer
-    def add(self) -> int:
+    def add(self, **kwargs) -> int:
         result = 0
+        wires = self.wires.copy()
         Q = deque(self.gates.items())
         while Q:
-            key = Q.popleft()
-            c, (f, a, b) = key
-            if (p := self.wires.get(a)) is None or (q := self.wires.get(b)) is None:
-                Q.append(key)
+            c, gate = Q.popleft()
+            if (p := wires.get(gate.a)) is None or (q := wires.get(gate.b)) is None:
+                Q.append((c, gate))
                 continue
-            r = f(p, q)
+            r = gate.f(p, q)
             if c[0] == "z":
                 result |= (r << int(c[1:]))
-            self.wires[c] = r
+            wires[c] = r
         return result
 
     def aligned(self) -> bool:
-        return self.z == self.add()
+        return self.z == self.add(time=False)
 
-    def validate(self) -> bool:
-        n = 100
-        for x in range(n):
-            for y in range(x, n):
-                self.__init__(x=x, y=y)
-                assert self.aligned(), f"{self.x=}, {self.y=}"
-        return True
+    def swap_outputs(self, a, b) -> None:
+        self.errors.update((a, b))
+        self.gates[a], self.gates[b] = self.gates[b], self.gates[a]
+
+    def debug_adder(self, idx: int) -> None:
+        X, Y, Z = [f"{r}{idx:02d}" for r in "xyz"]
+        # x ^ y = m
+        m = self.gates_[Gate("XOR", X, Y)]
+        # x & y = a
+        a = self.gates_[Gate("AND", X, Y)]
+        if idx == 0:
+            if a == Z:
+                return self.swap_outputs(a, m)
+            else:
+                return
+        if m == Z:
+            for (f_, m_, c_), v in self.gates_.items():
+                if f_ == "XOR" and (v == m_ or v == c_):
+                    b = self.gates_[Gate("AND", m_, c_)]
+                    if Gate("OR", a, b) in self.gates_:
+                        return self.swap_outputs(v, Z)
+        if a == Z:
+            for (f_, m_, c_), v in self.gates_.items():
+                if f_ == "XOR" and (m_ == m or c_ == m):
+                    return self.swap_outputs(v, Z)
+        # m ^ c = z
+        f, m_, c_ = self.gates[Z]
+        if f == "XOR" and m != m_ and m != c_:
+            return self.swap_outputs(a, m)
+        if f == "AND":
+            return self.swap_outputs(self.gates_[Gate("XOR", m_, c_)], Z)
+        if f == "OR":
+            for (f_, m_, c_), v in self.gates_.items():
+                if f_ == "XOR" and (m_ == m or c_ == m):
+                    return self.swap_outputs(v, Z)
+        # m & c = b
+        b = self.gates_[Gate("AND", m_, c_)]
+        if (c := self.gates_.get(Gate("OR", a, b))) is None:
+            for (f_, a_, b_), v in self.gates_.items():
+                if f_ == "OR" and (a_ == a or b_ == a):
+                    return self.swap_outputs(v, b)
+        if idx == self.bits - 1:
+            if c != (z := f"z{(idx + 1):02d}"):
+                return self.swap_outputs(c, z)
 
     @aoc_timer
-    def swap(self) -> str:
-        """By inspection of input versus correctly generated gates."""
-        swaps = [("shh", "z21"), ("dtk", "vgs"), ("dqr", "z33"), ("pfw", "z39")]
-        return ",".join(sorted(it.chain(*swaps)))
+    def debug(self) -> str:
+        _ = [self.debug_adder(idx) for idx in range(self.bits)]
+        assert self.aligned()
+        return ",".join(sorted(self.errors))
 
 
 def main() -> None:
     print("AoC 2024\nDay 24")
     system = System(path="input.txt")
     print("Part 1:", system.add())
-    print("Part 2:", system.swap())
+    print("Part 2:", system.debug())
 
 
 if __name__ == "__main__":
